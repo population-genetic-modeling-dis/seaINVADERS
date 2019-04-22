bcdc7de0f47adea6e78cd2b00eed15784075a0e5
Chris Bird <cbird808@gmail.com>
2019-04-22 11:50:06 -0500

    updated model.sh

--- model.sh
+++ model.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# Bash script used to run the Lionfish source model on HPC computer (5 nodes 100 CPUs)
 
-#SBATCH -J Number_LkasMarq          # Name of the job
-#SBATCH -o LkasMarq_number_%j.out       # Name of file that will have program output
-#SBATCH -e LkasMarq_number_%j.err       # Name of the file that will have job errors, if any
+#SBATCH -J SpInv          # Name of the job
+#SBATCH -o SpaceInvaders_%j.out       # Name of file that will have program output
+#SBATCH -e SpaceInvaders_%j.err       # Name of the file that will have job errors, if any
 #SBATCH -N 1           # Number of nodes ( the normal cluster partion has 22 total )
 #SBATCH -n 20              # Number of cores ( my test allocated 2 per node )
 #SBATCH -p normal           # Partition    
@@ -13,11 +12,10 @@
 module load openmpi
 module load R/openmpi/intel/3.2.2
 
-#To Run use command: sbatch Cope_ERGM_Bash.shn
+#To Run use command: sbatch model.sh NameOfParameters.R
 
-#mpirun -np 1 R CMD BATCH Marqmodel4_9.R $1
 TIME=$(date +%s%N | cut -b1-13)
-NEWDIR=output_${1}_$TIME
+NEWDIR=output_${1%.*}_$TIME
 mkdir $NEWDIR
 
-Rscript --verbose Marqmodel4_9.R $1 $NEWDIR > $NEWDIR/model.Rout
+Rscript --verbose model.R $1 $NEWDIR > $NEWDIR/model.Rout
